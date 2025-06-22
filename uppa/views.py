9cb8daaa2b6edb351a90d58fd414351661742787
JAPHES <japhesmurithi@gmail.com>
2025-06-22 19:51:23 +0300

    first commmit

---
+++ uppa/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'uppa/home.html')
+
+def about(request):
+    return render(request, 'uppa/about us.html')
+
+def departments(request):
+    return render(request, 'uppa/departments.html')
+
+def contact(request):
+    return render(request, 'uppa/contact us.html')
+
+def team(request):
+    return render(request, 'uppa/team.html')
