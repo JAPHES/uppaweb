49ae8e4a2626922fadbed7689dd44ea96ac6db4a
JAPHES <japhesmurithi@gmail.com>
2026-03-17 22:02:04 +0300

    updated redeploy on render

---
+++ staticfiles/js/main.js
@@ -0,0 +1,9 @@
+
+//the script on navbar.html file
+
+const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+      mobileMenu.classList.toggle('hidden');
+    });
