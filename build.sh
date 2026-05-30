d79d140a726a8c161785a27a850bd7e45c7704b9
JAPHES <japhesmurithi@gmail.com>
2026-05-30 22:52:22 +0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 pip install --upgrade pip
 pip install -r requirements.txt
 
+export DJANGO_SECRET_KEY="${DJANGO_SECRET_KEY:-build-time-secret-key}"
 python manage.py collectstatic --noinput
